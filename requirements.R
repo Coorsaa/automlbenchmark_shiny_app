7aaa6eeb16379c81dcbf346d72e497ed99020ad9
Stefan Coors <stefan.coors@gmx.net>
2021-12-13 17:26:04 +0100

    install requirements

---
+++ requirements.R
@@ -0,0 +1,41 @@
+packages = c(
+  "shiny",
+  "shinydashboard",
+  "shinyjs",
+  "plotly",
+  "shinyBS",
+  "shinyWidgets",
+  "markdown",
+  "shinythemes",
+  "DT",
+  "dplyr",
+  "tidyr",
+  "ggplot2",
+  "ggpubr",
+  "patchwork",
+  "OpenML",
+  "farff",
+  "BBmisc",
+  "stringr",
+  "scmamp",
+  "partykit"
+)
+
+for (p in packages) {
+  if (!requireNamespace(p, quietly = TRUE))
+    install.packages(p)
+}
+
+bioc_packages = c(
+  "BiocManager",
+  "BiocGenerics",
+  "Rgraphviz",
+  "graph",
+  "psychotree",
+  "psychotools"
+)
+
+for (p in bioc_packages) {
+  if (!requireNamespace(p, quietly = TRUE))
+    BiocManager::install(p)
+}
