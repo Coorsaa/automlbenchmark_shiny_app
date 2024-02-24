6534e148481f50fd910a0b0f58adece103cd1e73
PGijsbers <p.gijsbers@tue.nl>
2024-02-24 16:11:55 +0100

    Add training duration plot to stability page

--- streamlit-app/frontend/src/pages/stability.py
+++ streamlit-app/frontend/src/pages/stability.py
@@ -231,6 +231,149 @@ def plot_error_type_by_framework(error_counts, include_types: list[str]):
     st.pyplot(fig)
 
 
+def add_horizontal_lines(ax, lines: tuple[tuple[float, str], ...]):
+    """Draws horizontal lines specified by (y value, color)-pairs."""
+    for y, color in lines:
+        ax.axhline(y, color=color)
+
+
+def box_plot(data, metric=None, ylog=False, title="", ylim=None, figsize=(16, 9), with_framework_names=True,
+             add_counts=None, color_map=None):
+    """Creates a boxplot with data["frameworks"] on the x-axis and data[`metric`] on the y-axis
+
+    The figure's y-axis may be limited by `ylim` and the number of values outside this limit may be shown in the tick labels.
+    """
+    import seaborn
+    FRAMEWORK_TO_COLOR = {
+        "AutoGluon(B)": '#fe9900',
+        "AutoGluon(HQ)": '#fe7700',
+        "AutoGluon(HQIL)": '#fe5500',
+        "autosklearn": '#009f81',
+        "autosklearn2": '#00fccf',
+        "flaml": '#ff5aaf',
+        "GAMA(B)": '#8400cd',
+        "H2OAutoML": '#ffcb15',
+        "lightautoml": '#00c2f9',
+        "NaiveAutoML": '#c3c995',
+        "MLJAR(B)": '#ffb2fd',
+        "MLJAR(P)": '#ddb2fd',
+        "RandomForest": "#e20134",
+        "TPOT": '#9f0162',
+        "TunedRandomForest": '#c4a484',
+    }
+
+    def is_old(framework: str, constraint: str, metric: str) -> bool:
+        """Encodes the table in `raw_to_clean.ipynb`"""
+        if framework == "TunedRandomForest":
+            return True
+        if constraint == "1h8c_gp3":
+            return False
+        if framework in ["autosklearn2", "GAMA(B)", "TPOT"]:
+            return True
+        return framework == "MLJAR(B)" and metric != "neg_rmse"
+
+    if add_counts and (add_counts != "outliers" and not isinstance(add_counts, dict)):
+        raise ValueError("`add_counts` must be 'outliers' or a dictionary mapping each framework to a number.")
+
+    color_map = color_map or FRAMEWORK_TO_COLOR
+    color_map = {k: v for k, v in color_map.items() if k in data["framework"].unique()}
+
+    metric = metric or data.metric.unique()[0]
+    if metric.startswith("neg_"):
+        pos_metric = metric[len("neg_"):]
+        data[pos_metric], metric = -data[metric], pos_metric
+
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+    seaborn.boxplot(
+        data=data,
+        x="framework",
+        y=metric,
+        order=color_map,
+        hue="constraint" if data.constraint.nunique() > 1 else None,
+        palette=color_map if data.constraint.nunique() == 1 else None,
+        ax=ax,
+        fliersize=1,
+    )
+
+    if ylog:
+        ax.set_yscale("log")
+
+    ax.set_ylabel(metric, size='xx-large')
+    ax.set_xlabel("")
+    ax.tick_params(axis='both', which='both', labelsize=18)
+
+    if title:
+        ax.set_title(title, fontsize=18)
+
+    # Dirty hack for displaying outliers, we overlap minor and major tick labels, where
+    # minor labels are used to display the number of outliers, and major tick labels may
+    # be used to display the framework names.
+    constraint = data.constraint.unique()[0]
+    smetric = data.metric.unique()[0]
+    frameworks = color_map.keys()
+    frameworks = [
+        f"{fw if with_framework_names else ''}*" if is_old(fw, constraint, smetric) else fw
+        for fw in frameworks
+    ]
+    if add_counts:
+        # There will be minor tick labels displayed for outliers,
+        # to avoid rendering on top of each other, we offset the label location
+        # with a dirty hack of using leading spaces :-)
+        frameworks = [f"   {fw}" for fw in frameworks]
+    ax.tick_params(axis="x", which="major", rotation=-90)
+    ax.set_xticks(*zip(*enumerate(frameworks)))
+
+    if ylim:
+        ax.set_ylim(ylim)
+        if add_counts != "outliers":
+            print("Warning! Ylim is set but outliers are not reported.")
+
+    counts = []
+    if add_counts:
+        if add_counts == "outliers":
+            add_counts = {}
+            for framework in color_map:
+                framework_outliers = data[(data["framework"] == framework) & (data[metric] < ylim[0])]
+                add_counts[framework] = f"{len(framework_outliers)}"
+
+        # We need to offset the minor tick labels, otherwise they won't render.
+        ax.set_xticks(
+            ticks=[i - 0.01 for i in range(len(color_map))],
+            labels=[f"[{add_counts.get(f, 'x')}]" for f in color_map],
+            minor=True
+        )
+
+    return fig, ax
+
+
+def plot_train_duration_for_constraint(constraint: str, results):
+    time_budget = get_print_friendly_name(constraint)
+    data = results[results["constraint"] == constraint]
+    data = data[~data["framework"].isin(["constantpredictor"])]
+
+    data["timeout"] = data["info"].apply(lambda msg: isinstance(msg, str) and "Interrupting thread MainThread" in msg)
+    timeout_counts = dict(data[["framework", "timeout"]].groupby("framework").sum()["timeout"])
+    filename = f"train-duration-{constraint}.pdf".replace(" ", "-")
+    print("generating", filename)
+
+    time_limit = 3600 if constraint == "1h8c_gp3" else 3600 * 4
+    fig, ax = box_plot(
+        data,
+        metric="training_duration",
+        title=f"Training Duration {time_budget}",
+        # ylim=[-3 if metric != "auc" or constraint !="4h8c_gp3" else -7, 0],
+        figsize=(6, 3),
+        add_counts=timeout_counts,
+        with_framework_names=True,  # ttype == "regression",
+    )
+    add_horizontal_lines(ax, ((time_limit, "grey"), (time_limit + 3600 + 1200, "red")))
+    ax.set_ylabel("Training Duration")
+    if constraint == "1h8c_gp3":
+        ax.set_yticks([3600, 7200], ["1H", "2H"])
+    if constraint == "4h8c_gp3":
+        ax.set_yticks([3600 * 4, 3600 * 5 + 1200], ["4H", "5H"])
+    st.pyplot(fig)
+
 if __name__ == "__main__":
     st.write(
         """
@@ -349,3 +492,19 @@ if __name__ == "__main__":
         We tested the frameworks at two different time constraints: 1 hour and 4 hours.
         """
     )
+    left, right = st.columns([0.8, 0.2])
+    with right:
+        const = st.selectbox(
+            label="Time Constraint",
+            options=["1 hour", "4 hours"],
+            index=0,
+        )
+        types = st.multiselect(
+            label="Task Types:",
+            options=list(st.session_state.filtered_results["type"].unique()),
+            default=list(st.session_state.filtered_results["type"].unique()),
+        )
+    with left:
+        ree= st.session_state.filtered_results[st.session_state.filtered_results["type"].isin(types)]
+        const_map = {"1 hour": "1h8c_gp3", "4 hours":"4h8c_gp3"}
+        plot_train_duration_for_constraint(const_map[const], ree)
