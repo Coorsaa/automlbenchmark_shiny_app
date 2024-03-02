2c127a74b41e56eee7dc0050d744afd0e150ce5e
PGijsbers <p.gijsbers@tue.nl>
2024-03-02 12:55:36 +0100

    Add HTML card for warning to allow link to Github issues

--- streamlit-app/frontend/src/home.py
+++ streamlit-app/frontend/src/home.py
@@ -19,26 +19,37 @@ def configure_streamlit():
         layout="wide",
     )
 
-st.write(
-    """# About This App
-    
-    ⚠️ Warning
-    This app was not part of the JMLR peer review process. If you find any mistakes,
-    please open a Github issue. We welcome contributions.
-""")
-# st.markdown('<div style="color:blue;background-color:red;">hello</div>', unsafe_allow_html=True)
+st.write("# AutoML Benchmark")
+
 st.write("""
-    
+
 This app allows you to further inspect the results presented in the JMLR paper
 "AMLB: an AutoML Benchmark" by Gijsbers et al. (2024). The paper discusses the benchmark design and its limitations,
 and contextualizes the results. We strongly encourage you to first read the paper before
-drawing any conclusions. This app includes all the figures of the paper, with some
-additional controls which let you look at specific aspects of the data:
+drawing any conclusions. 
 """)
 
+with open("src/html/card.html", 'r') as fh:
+    card_html = fh.read()
+
+warning_html = card_html.replace(
+    "ICON", "⚠️"
+).replace(
+    "HEADER", "Important"
+).replace(
+    "BODY", """This app was not part of the JMLR peer review process. If you find any mistakes,
+please open a <a href="#">Github issue</a>. We welcome contributions."""
+)
+st.markdown(
+    warning_html,
+    unsafe_allow_html=True,
+)
+
+st.write("This app includes all the figures of the paper, with some"
+         " additional controls which let you look at specific aspects of the data:")
 st.page_link("home.py", label=" * Figure 1: Benchmarking Suite")
 st.page_link("pages/cd_diagram.py", label=" * Figure 2: Critical Difference Diagrams")
-st.page_link("home.py", label=" * Figure 3 and 4: Scaled Performance Boxplots")
+st.page_link("pages/performance.py", label=" * Figure 3 and 4: Scaled Performance Boxplots")
 st.page_link("home.py", label=" * Figure 5: Bradley-Terry Trees")
 st.page_link("pages/inference.py", label=" * Figure 6 and 7: Model Inference Time")
 st.page_link("pages/stability.py", label=" * Figure 8 and 9: Errors and training duration.")
